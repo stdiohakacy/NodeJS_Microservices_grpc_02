0efe069cbe456f6efde9133ddbfa57f779551192
Duy <huynhduytam2210@gmail.com>
2020-02-28 21:03:46 +0700

    initial grpc

--- server/server.js
+++ server/server.js
@@ -1,22 +1,44 @@
-var PROTO_PATH = __dirname + '/hello.proto';
-var grpc = require('grpc');
-var protoLoader = require('@grpc/proto-loader');
-var packageDefinition = protoLoader.loadSync(
-    PROTO_PATH,
-    {keepCase: true,
-     longs: String,
-     enums: String,
-     defaults: true,
-     oneofs: true
-    });
-var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
-function sayHello(call, callback) {
-  callback(null, {message: 'Hello ' + call.request.name});
+const PROTO_PATH = __dirname + '/notes.proto';
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
+
+const packageDefinition = protoLoader.loadSync(
+    PROTO_PATH, {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    }
+);
+
+const notes = [{
+        id: '1',
+        title: 'Note 1',
+        content: 'Content 1'
+    },
+    {
+        id: '2',
+        title: 'Note 2',
+        content: 'Content 2'
+    }
+]
+
+let note = grpc.loadPackageDefinition(packageDefinition).noteservice;
+
+function list(call, callback) {
+    callback(null, {notes});
 }
+
 function main() {
-  var server = new grpc.Server();
-  server.addService(hello_proto.Greeter.service, {sayHello: sayHello});
-  server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
-  server.start();
+    let server = new grpc.Server();
+
+    server.addService(note.NoteService.service, {
+        list: list
+    });
+
+    server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
+    server.start();
 }
+
 main();
